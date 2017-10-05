a4fa0bb2d5a63297836b1d62f3a325c6753e1d49
esthertan <esthertan90@hotmail.com>
2017-10-05 19:22:04 +0800

    New Setup Esther

--- BusTracker/app/src/main/java/com/myxlab/bustracker/BaseActivity.java
+++ BusTracker/app/src/main/java/com/myxlab/bustracker/BaseActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by MyXLab on 30/5/2017.
+ * Created by MyXLab on 30/5/2017. ..
  */
 public class BaseActivity extends AppCompatActivity {
 
