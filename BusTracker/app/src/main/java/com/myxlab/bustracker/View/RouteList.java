a15c2000f6c07156ea9704ef920c0c043b3815db
Esthertan90 <p72765@siswa.ukm.edu.my>
2018-01-12 16:44:02 +0800

    Add color on status bar for certain activity

--- BusTracker/app/src/main/java/com/myxlab/bustracker/View/RouteList.java
+++ BusTracker/app/src/main/java/com/myxlab/bustracker/View/RouteList.java
@@ -2,14 +2,25 @@ package com.myxlab.bustracker.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.myxlab.bustracker.R;
 
 public class RouteList extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journey);
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorAccent));
+        }
     }
 }
